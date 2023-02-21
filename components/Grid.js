25eeb5197336c3f0af7b112b3a016310b769341f
shineljin <psyruz18@gmail.com>
2023-02-21 13:49:31 +0800

    added homepage hover

--- components/Grid.js
+++ components/Grid.js
@@ -7,17 +7,19 @@ const Grid = ({ data }) => {
         data.map((item) => {
           return (
             <Link href={`/details/${item.id}`} key={item.id}>
-              <div className="group hover:scale-105 transition-all">
-                <img
-                  src={item.image}
-                  alt={item.id}
-                  className="w-full h-[150px] xs:h-[250px] lg:h-[350px]  md object-cover cursor-pointer"
-                  width={200}
-                  loading="lazy"
-                />
-                <h1 className="mx-auto text-center text-gray-300 text-sm lg:text-base group-hover:underline cursor-pointer">
-                  {item.title.userPreferred.slice(0, 35)}
-                </h1>
+              <div className="group transition-all duration-300 bg-gradient-to-b from-primary-main via-pink-400 to-secondary-main hover:shadow-2xl hover:shadow-[#00ff754d] ">
+                <div className="card2 bg-bg-main transition-all duration-300 hover:scale-[0.98]  ">
+                  <img
+                    src={item.image}
+                    alt={item.id}
+                    className="w-full h-[150px] xs:h-[250px] lg:h-[350px]  md object-cover cursor-pointer"
+                    width={200}
+                    loading="lazy"
+                  />
+                  <h1 className="mx-auto text-center text-gray-300 text-sm lg:text-base group-hover:underline cursor-pointer">
+                    {item.title.userPreferred.slice(0, 35)}
+                  </h1>
+                </div>
               </div>
             </Link>
           );
