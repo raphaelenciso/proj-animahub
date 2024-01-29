3cf3645fc5f871236ddf388bcc75f80e64a24d74
raphaelenciso <psyruz18@gmail.com>
2024-01-29 20:23:18 +0800

    test

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 export const provider = process.env.NEXT_PUBLIC_PROVIDER || "gogoanime";
-export const api = "https://animahub-api.vercel.app/anime/gogoanime/";
+export const api =
+  provider === "gogoanime"
+    ? "https://animahub-api.vercel.app/anime/gogoanime/"
+    : "https://animahub-api.vercel.app/meta/anilist/";
 
 // export const provider = "anilist";
 // export const api = "https://animahub-api.vercel.app/meta/anilist/";
