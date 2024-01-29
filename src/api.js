d7b7e0d370a2901d977cca8ab48899ee29bcd820
raphaelenciso <psyruz18@gmail.com>
2024-01-29 20:22:45 +0800

    dynamic provider

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const provider = "gogoanime";
+export const provider = process.env.NEXT_PUBLIC_PROVIDER || "gogoanime";
 export const api = "https://animahub-api.vercel.app/anime/gogoanime/";
 
 // export const provider = "anilist";
